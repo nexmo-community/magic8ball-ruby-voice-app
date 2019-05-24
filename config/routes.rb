1ef2ad1ef1cb044138189ce879bae37a8e4111d4
Lauren Lee <Laurenelee88@gmail.com>
2019-05-24 10:41:49 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/answer', to: 'responses#answer'
 
-  post '/event', to: 'responses#event'
-
-  post '/dtmf', to: 'responses#dtmf'
-
   post '/caller_input', to: 'responses#caller_input'
 
   post '/event', to: 'responses#event'
